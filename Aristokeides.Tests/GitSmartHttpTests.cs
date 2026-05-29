9ce86fcac9b25868e76f1c29d76b0773722fe034
muzixholic <chm1610@gmail.com>
2026-05-29 14:40:26 +0900

    test(02-01): create stub test files RepositoriesControllerTests.cs and GitSmartHttpTests.cs

---
+++ Aristokeides.Tests/GitSmartHttpTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aristokeides.Tests
+{
+    public class GitSmartHttpTests
+    {
+        [Fact]
+        public void Stub_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Aristokeides.Tests/RepositoriesControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aristokeides.Tests
+{
+    public class RepositoriesControllerTests
+    {
+        [Fact]
+        public void Stub_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
